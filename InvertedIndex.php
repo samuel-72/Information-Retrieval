403173b4dad743de457f64f513077d63c1b0ac0f
Vikash Raja Samuel Selvin <samuel-72@users.noreply.github.com>
2016-03-04 21:00:06 -0800

    Update InvertedIndex.php

--- InvertedIndex.php
+++ InvertedIndex.php
@@ -2,115 +2,81 @@
 
 ini_set('error_reporting', E_ALL);
 
-echo nl2br ("This program prints an inverted index for a set of documents.\n");
-echo nl2br ("This program strips all punctuation characters from the text for the purpose of creating an inverted index.\n");
+echo  ("This program prints an inverted index for a set of documents".PHP_EOL.PHP_EOL);
+echo  ("This program strips all punctuation characters from the text for the purpose of creating an inverted index.\n".PHP_EOL.PHP_EOL);
 
 $document_id = 0;
-$termFrequency = array();
-$termDocMapping = array();
-$termDocPositionMapping = array();
-$termDocPositions = array();
-	
+$termFrequency = [];
+$termDocMapping = [];
+$termDocPositionMapping = [];
+    
 function createInvertedIndex($dirName)
 {
-    global $document_id, $termFrequency, $termDocMapping, $termDocPositions, $termDocPositionMapping;	
-	
-    echo nl2br("\n\nThe directory name provided is - " . $dirName . "\n\n" );
-    $files = glob($dirName."*");
-	
+    global $document_id, $termFrequency, $termDocMapping, $termDocPositions, $termDocPositionMapping;    
+    
+    echo (PHP_EOL . PHP_EOL . "The directory name provided is - " . $dirName . PHP_EOL . PHP_EOL  );
+    $files = glob(rtrim($dirName)."/*.txt");
+    
     if ( count($files) < 1) {
-	    echo nl2br("There are no files in the given directory.\n\nProgram will exit now.\n\n");
-	    exit();
+        echo ("There are no files in the given directory.\n\nProgram will exit now." . PHP_EOL . PHP_EOL );
+        exit();
     }
-	
+    
     foreach( $files as $fname ) {
-	    echo nl2br( "\n\nFilename - " . $fname . "\n\n") ;
+        echo ( "\n\nFilename - " . $fname . PHP_EOL . PHP_EOL) ;
 
-	    $fileContentsWithPunctuations = explode(' ',file_get_contents($fname));
-		
-	    $fileContents = preg_replace("#[[:punct:]]#", "", $fileContentsWithPunctuations);
-		
-	    $position = 0;
-	    $termDocPositions = array();
-		
-	    foreach ($fileContents as $word) {
-			//echo ("Word : " . $word . "\tat pos : " . $position );
-			
-			// Get the term frequency
-		    if ( !array_key_exists( $word,$termFrequency ) ) {
-			    //$termDocMapping[$word]  = (string)$document_id;
-			    $termFrequency[$word] = 1;
-		    }
-		    else {
-			    $termFrequency[$word] += 1;
-		    }
-						
-			// Get the term mapped with the document id, this will be used for finding the no of docs the term is associated with
-		    if ( !array_key_exists( $word,$termDocMapping ) ) {
-			    //$termDocMapping[$word]  = (string)$document_id;
-			    $termDocMapping += array( $word => (string)$document_id  );
-		    }
-		    else {
-			    $termDocMapping[$word]  = $termDocMapping[$word] . "," .(string)$document_id;
-		    }
-			
-			// Get all positions where the word occurs in the document
-		    if ( !array_key_exists( $word,$termDocPositions ) ) {
-				//$termDocMapping[$word]  = (string)$document_id;
-			    $termDocPositions += array( $word => (string)$position  );
-		    }
-		    else {
-			    $termDocPositions[$word]  = $termDocPositions[$word] . "," .(string)$position;
-		    }
-			
-		    $position += 1;
-	    }
-		
-	    foreach( $termDocPositions as $terms => $positions ) {
-			//echo nl2br("\nTerm : " . $terms);
-			//echo (" - " . $termDocPositions[$terms] . ",");
-			
-		    if ( !array_key_exists( $terms,$termDocPositionMapping ) ) {
-			    $termDocPositionMapping[$terms] = (array($document_id => array($termDocPositions[$terms]) ));
-		    }
-		    else {
-			    $termDocPositionMapping[$terms] += (array($document_id => array($termDocPositions[$terms]) ));
-		    }
-			//var_dump($termDocPositionMapping);
-	    }
-		
-	    $document_id += 1;
-		
-		//echo nl2br("The file content is : \n" . $string);
+        $fileContentsWithPunctuations = explode(' ',file_get_contents($fname));
+        
+        $fileContents = preg_replace("#[[:punct:]]#", "", $fileContentsWithPunctuations);
+        
+        $position = 0;
+        $termDocPositions = [];
+        
+        foreach ($fileContents as $word) {          
+            // Get the term frequency
+            if ( !array_key_exists( $word,$termFrequency ) ) {
+                $termFrequency[$word] = 1;
+            }
+            else {
+                $termFrequency[$word] += 1;
+            }
+                        
+            // Get the term mapped with the document id, this will be used for finding the no of docs the term is associated with
+            if ( !array_key_exists( $word,$termDocMapping ) ) {
+                $termDocMapping[$word] = [$document_id]  ;
+            }
+            else {
+                $termDocMapping[$word][]  = $document_id;
+            }
+            // Get all positions where the word occurs in the document
+            if ( !array_key_exists( $word,$termDocPositions ) ) {
+                $termDocPositions[$word] = (string)$position ;
+            }
+            else {
+                $termDocPositions[$word]  .= "," . (string)$position ;
+            }            
+            $position += 1;
+        }
+        foreach( $termDocPositions as $terms => $positions ) {
+            if ( !array_key_exists( $terms,$termDocPositionMapping ) ) {
+                $termDocPositionMapping[$terms] = [$document_id => $termDocPositions[$terms]];
+            }
+            else {
+                $termDocPositionMapping[$terms] += [$document_id => $termDocPositions[$terms]];
+            }
+        }
+        $document_id += 1;
     }
-	
-    echo nl2br("\n\nGoing to print all term - doc - positions \n\n");
-    foreach($termDocPositionMapping as $terms => $doc_pos) {
-		//echo nl2br("\nTerm : " . $terms . " Num_Docs : " . count($doc_pos) . " Freq : " . $termFrequency[$terms] . " : ");
-	    echo nl2br("\n" . $terms . " : " . count($doc_pos) . " : " . $termFrequency[$terms] . " : ");
-	    foreach( $doc_pos as $doc => $positions) {
-		    echo ( "( " . $doc . " , ");
-		    $p = "";
-		    foreach($positions as $pos) {
-			    $p = $p . $pos . ", ";
-				//echo nl2br( $pos. "," );
-		    }
-			//$p = rtrim($p,",");
-			//echo ("th" . substr($p,0,-2) . "th");
-		    echo ( substr($p,0,-2) . " ), " );
-	    }
+    echo (PHP_EOL . PHP_EOL . "Please find below the inverted index" .PHP_EOL . PHP_EOL );
+
+    foreach($termDocPositionMapping as $terms => $docs) {
+        echo (PHP_EOL . $terms . ":" . count($docs) . ":" . $termFrequency[$terms] . ":");
+        $docPositions = "";
+        foreach($docs as $doc => $positions) {
+            $docPositions .= ( "(" . $doc . "," . $positions . ")," );
+        }
+        echo (rtrim($docPositions,','));
     }
-	
-	/*
-	foreach($termDocMapping as $terms => $docid)
-	{
-		echo nl2br("\nTerm : " . $terms);
-		echo ("    - " . $termDocMapping[$terms] . ",");
-		
-	}
-	
-	var_dump($termDocMapping);
-	*/
 }
 
 // Read the directory name from the command line
